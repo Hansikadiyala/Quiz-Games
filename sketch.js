ffafb9532021d9725d48608553410a72548acfda
Hansikadiyala <64314013+Hansikadiyala@users.noreply.github.com>
2020-12-09 18:13:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var canvas, backgroundImage;
+var playerName,gameState=0,playerCount
+var database;
+
+var form, player, game;
+
+
+function preload(){
+ 
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 1){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
+  console.log(playerName);
+}
